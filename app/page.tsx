19924614aa7b5effdc2912264d4c079f80f86ac0
Pablo Almonte <pabloalmontedev@gmail.com>
2023-03-29 06:33:53 -0400

    feat: initial design for the UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import Image from "next/image"
+
+function getCommits() {
+  return fetch('https://api.github.com/repos/PabloAlmonte/BoxCodes/commits').then(res => res.json())
+}
+
+export default async function Home() {
+  const commits = await getCommits()
+
   return (
-    <main>
-      <h1>Github commits</h1>
+    <main className="container mx-auto">
+      <Image className="rounded-full mx-auto my-5" src='/assets/images/logo.png' alt='Github commits logo' width={200} height={200} />
+      {
+        commits.map((commit) => (
+          <div className="mb-3 border-b pb-3" key={commit.sha}>
+            <h1 className="font-bold">{commit.commit.message}</h1>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <Image className="rounded-full" src={commit.author?.avatar_url} alt="Author Image" width={30} height={30} />
+                <h6 className="text-base ml-1">{commit.commit.author.name}</h6>
+              </div>
+              <small>{commit.commit.author.date}</small>
+            </div>
+          </div>
+        ))
+      }
     </main>
   )
 }
